be9c6c29196ec2934cb141cb83c17c39a49cc8cf
newsonaut <mwrogers@icloud.com>
2026-06-19 14:49:10 -0700

    new post, various

--- eleventy.config.js
+++ eleventy.config.js
@@ -10,6 +10,22 @@ export default function (eleventyConfig) {
   eleventyConfig.setOutputDirectory("public");
 
   eleventyConfig.addPlugin(rssPlugin);
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return new Date(dateObj).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      timeZone: "UTC",
+    });
+  });
+  eleventyConfig.addFilter("getAllTags", (collection) => {
+    let tagSet = new Set();
+    for (let item of collection) {
+      (item.data.tags || []).forEach((tag) => tagSet.add(tag));
+    }
+    return Array.from(tagSet).sort();
+  });
 }
 
 export const config = {
---
+++ src/blog/personal/birthday.md
@@ -0,0 +1,11 @@
+---
+title: The Accident of My Birthday
+date: 2026-06-19
+tags: ["Personal"]
+---
+
+I was born, fortuitously, at one second past midnight on January the first in the year 1800. Thus, my age has always reflected the year. This was as true in the 19th century as it is in the current 21st century.
+
+I bring this up because it may be a clue. For it was at one second past midnight on the day I turned 26 that I suddenly found myself exactly 200 years into the future.
+
+I’m not sure what to make of it, but I am convinced that the marvelous new technologies of this era may aid me. This web log is an experimentation to that end.
